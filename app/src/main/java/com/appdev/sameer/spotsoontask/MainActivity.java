c0c97fa830386ba476a55b65b106cb319fc69305
sameer <md.sameerahmed8@gmail.com>
2017-06-13 12:52:29 +0530

    Final Commit

--- app/src/main/java/com/appdev/sameer/spotsoontask/MainActivity.java
+++ app/src/main/java/com/appdev/sameer/spotsoontask/MainActivity.java
@@ -1,13 +1,185 @@
 package com.appdev.sameer.spotsoontask;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.appdev.sameer.spotsoontask.fragments.ImagesFragment;
+import com.appdev.sameer.spotsoontask.fragments.MilestoneFragment;
+import com.appdev.sameer.spotsoontask.fragments.VideoFragment;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ViewPagerEx.OnPageChangeListener, BaseSliderView.OnSliderClickListener {
+    private SliderLayout mDemoSlider;
+    ArrayList<String> imageUrls;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private Toolbar toolbar;
+    Context context;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private int[] tabIcons = {R.drawable.video, R.drawable.image, R.drawable.milestone};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = MainActivity.this;
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitle("Home");
+
+
+        mDemoSlider = (SliderLayout) findViewById(R.id.slider);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+
+        tabLayout.setOnTabSelectedListener(
+                new TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
+
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        super.onTabSelected(tab);
+                        int tabIconColor = ContextCompat.getColor(context, R.color.colorPrimary);
+                        tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+                        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.colorPrimary));
+
+                    }
+
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+                        super.onTabUnselected(tab);
+                        int tabIconColor = ContextCompat.getColor(context, R.color.textColorGray);
+                        tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+                        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.textColorGray));
+
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+                        super.onTabReselected(tab);
+                    }
+                }
+        );
+
+
+        imageUrls = new ArrayList<>();
+        imageUrls.add("https://i.ytimg.com/vi/PvyzEvbaXMk/maxresdefault.jpg");
+        imageUrls.add("https://i.ytimg.com/vi/8Tp_92hmcOw/maxresdefault.jpg");
+        imageUrls.add("http://www.stylefashionista.com/wp-content/uploads/2015/06/cute-girl-listening-music-wallpaper_202074282.jpg");
+        imageUrls.add("https://www.missmalini.com/wp-content/uploads/2015/01/maxresdefault1.jpg");
+
+        for (int i = 0; i < imageUrls.size(); i++) {
+
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView
+                    .image(imageUrls.get(i))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+            mDemoSlider.addSlider(textSliderView);
+
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setDuration(6000);
+        mDemoSlider.addOnPageChangeListener(this);
+
     }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this, "Slider Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new VideoFragment(), "Videos");
+        adapter.addFragment(new VideoFragment(), "Images");// ImagesFragment()
+        adapter.addFragment(new VideoFragment(), "Milestone");// MilestoneFragment()
+
+        viewPager.setAdapter(adapter);
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
